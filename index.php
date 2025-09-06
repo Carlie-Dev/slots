c5810e331c500064b654878f603ad5ab8abd5ef4
Carlie-Dev <ellepheonixgames@gmail.com>
2025-09-05 23:00:54 -0500

    tested while loop

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<php
+<?php
+$slots = array("A","B","C");
+
+$maxwinning = 500;
+$maxspins = 20;
+echo implode(",", $slots) . "\n";
+$i = 0;
+$wins = 0;
+
+while($i!=$maxspins or $wins <= 100 ){
+    echo "hi\n";
+    $i++;
+    $wins += 10; 
+}
