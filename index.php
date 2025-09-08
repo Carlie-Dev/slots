dbefb46dd183a38bd0e9ab416b7a30c21e3d3c8f
Carlie-Dev <ellepheonixgames@gmail.com>
2025-09-07 22:46:51 -0500

    Set up match & fixed number of errors

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <?php
 $slots = array("A","B","C");
 
-$maxwinning = 500;
+$maxwinnings = 500;
 $maxspins = 20;
 echo implode(",", $slots) . "\n";
 $i = 0;
 $wins = 0;
 
-while($i!=$maxspins or $wins <= 100 ){
-    echo "hi\n";
+while($i<=$maxspins and $wins <= $maxwinnings) {
+
+    $spin =[];
+    for($j = 0; $j < 3; $j++){
+        // picks a random slot in the array
+        $spin[] = $slots[array_rand($slots)];
+    }
+    $condensedSpin = implode($spin);
+    echo $condensedSpin . "\n";
+
+    $payout = match($condensedSpin) {
+        "AAA","BBB","CCC" => 100,
+        "AAB","ABA","BAA","ABB","BBA","BAB","BCC","CBC","CCB","ACC","CAC","CCA" => 50,
+        2 => 20,
+        default => 0
+    };
+
     $i++;
-    $wins += 10; 
+    $wins += $payout; 
 }
+echo "Total spins: $i\n";
+echo "Total winnings: $wins\n";
